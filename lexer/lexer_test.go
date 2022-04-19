82f3579763253a51fd6923ffc0af58f5e8df937b
GS.Hyeon <gshyeon@testworks.co.kr>
2022-04-19 18:29:05 +0900

    Add test for lexing variable name with digit

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -9,6 +9,7 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
+let ten2 = 10;
 
 let add = fn(x, y) {
   x + y;
@@ -44,6 +45,11 @@ if (5 < 10) {
 		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
 		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten2"},
 		{token.ASSIGN, "="},
 		{token.INT, "10"},
 		{token.SEMICOLON, ";"},
