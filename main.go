aa8d7ec81a744566835106f1d02fd41e5c92bf98
GS.Hyeon <gshyeon@testworks.co.kr>
2022-04-19 18:09:44 +0900

    refactor: remove unnecessary code

--- main.go
+++ main.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 	"monkey/repl"
 	"os"
-	user2 "os/user"
+	"os/user"
 )
 
 func main() {
-	user, err := user2.Current()
+	usr, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Hello %s! This is Monkey programming language!\n", user.Username)
+	fmt.Printf("Hello %s! This is Monkey programming language!\n", usr.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
